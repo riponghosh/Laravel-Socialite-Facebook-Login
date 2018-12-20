2774d6438fbea5b90eeb0c39e417f8de5b18fcea
Ripon Ghosh <ripon.ghosh200@gmail.com>
2018-12-20 17:41:01 +0600

    email  show and must php artisan cache:config in every env change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
                     You are logged in!
                     @if($service=='facebook')
                         <div class="title m-b-md">
-                            Welcome {{ $details->user['name']}} !.
+                            Welcome {{ $details->user['name']}} ! <br> Your email is : {{ $details->user['email']}}.
                         </div>
                     @endif
                 </div>
